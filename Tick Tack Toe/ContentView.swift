db7bb19a791e3566a6a04828aac5efabcead525c
hosam mohamed <hosamelmalt@gmail.com>
2020-11-25 13:20:40 -0800

    Initial Commit

---
+++ Tick Tack Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tick Tack Toe
+//
+//  Created by Hossam on 11/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tick Tack Toe/Tick_Tack_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tick_Tack_ToeApp.swift
+//  Tick Tack Toe
+//
+//  Created by Hossam on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct Tick_Tack_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
